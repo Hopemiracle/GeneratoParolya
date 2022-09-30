3446799f3ccb3f0ad0ea3d58ce1e8dc90955a4d2
user <user@526-9>
2022-09-30 08:51:53 +0400

    Добавьте файлы проекта.

---
+++ Вас/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Вас
+{
+    internal class Program
+    {
+        
+        static string numbers = "123456789";
+        static string letters = "QWERTYRIUYOUIPYZXCVBNMKJIOLPD";
+
+        static char generate_random_letter()
+        {
+            Random rnd = new Random();
+            return letters[rnd.Next(letters.Length)];
+
+        }
+        static char generate_random_number()
+        {
+            Random rnd = new Random();
+            return numbers[rnd.Next(numbers.Length)];
+        }
+
+        public static void Main(string[] args)
+        {
+            int password_length = 10;
+            string password = "";
+            
+
+            for (int i = 0; i < password_length; i++)
+            {
+                if( i % 2 == 0)
+                {
+                    password += generate_random_letter();
+
+                }
+                else
+                {
+                    password += generate_random_number();
+                }
+                Console.WriteLine("Пароль");
+                Console.WriteLine(password);
+            }
+        }
+
+    }
+}
